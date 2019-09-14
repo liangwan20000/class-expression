1a2047a300ccd5031bcb1925314b9672a09ac744
张浩123 <13930333639@163.com>
2019-09-14 18:27:41 +0800

    点标记语法

--- src/App.js
+++ src/App.js
@@ -5,12 +5,29 @@ import './App.css';
 // 类表达式
 import Classexopression from './components/class-expression.js';
 
-function App() {
-	return (
-		<div>
-			<Classexopression name='张三'></Classexopression>
-		</div>
-	);
+class App extends React.Component {
+	// 构造函数
+	constructor (props) {
+		// 调用父类构造函数
+		super(props)
+		this.state = {
+			flag: false
+		}
+	}
+	componentDidMount () {
+		this.setState({
+			flag: true
+		})
+	}
+	render () {
+		return (
+			<div>
+				<Classexopression.Header></Classexopression.Header>
+				<Classexopression.Content></Classexopression.Content>
+				<Classexopression.Footer></Classexopression.Footer>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/class-expression.js
+++ src/components/class-expression.js
@@ -1,24 +1,41 @@
 /**
-*	类表达式
+*	点标记语法
 */
 // 引入React核心文件
 import React from 'react';
-// 创建类组件
-class Person {
-	// 调用构造函数，并接收参数
-	constructor (name) {
-		// 设置属性
-		this.name = name
-	}
+// 注意表达式不会被提升，所以必须在对象之前定义
+let Header = class extends React.Component {
 	// 当前类组件导出的内容由render提供
 	render () {
 		return (
-			<div>{ this.name }</div>
+			<h1>头部</h1>
+		)
+	}
+};
+let Content = class extends React.Component {
+	// 当前类组件导出的内容由render提供
+	render () {
+		return (
+			<h1>内容区域</h1>
+		)
+	}
+};
+let Footer = class extends React.Component {
+	// 当前类组件导出的内容由render提供
+	render () {
+		return (
+			<h1>底部</h1>
 		)
 	}
 }
+// 创建页面对象Html
+let Html = {
+	Header: Header,
+	Content: Content,
+	Footer: Footer
+};
 
 /**
 *	导出类组件
 */
-export default Person;
+export default Html;
