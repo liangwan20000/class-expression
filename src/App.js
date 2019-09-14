cb23dae23a156800f5f930000060800239795317
张浩123 <13930333639@163.com>
2019-09-14 19:43:14 +0800

    类延展操作

--- src/App.js
+++ src/App.js
@@ -3,28 +3,25 @@ import './App.css';
 
 // 导入组件
 // 类表达式
-import Classexopression from './components/class-expression.js';
+// import Classexopression from './components/class-expression.js';
+// 类延展操作
+import Class from './components/parameter.js';
 
 class App extends React.Component {
-	// 构造函数
-	constructor (props) {
-		// 调用父类构造函数
-		super(props)
-		this.state = {
-			flag: false
-		}
-	}
-	componentDidMount () {
-		this.setState({
-			flag: true
-		})
-	}
 	render () {
+		let info = {
+			name: '张三',
+			age: 20,
+			gender: '男',
+			photo: 120
+		};
+		// 这样可以取出不需要的数据，只传递需要的数据
+		let { name, ...other } = info;
 		return (
 			<div>
-				<Classexopression.Header></Classexopression.Header>
-				<Classexopression.Content></Classexopression.Content>
-				<Classexopression.Footer></Classexopression.Footer>
+				{/*将info属性传到子组件*/}
+				<Class { ...info }></Class>
+				<Class { ...other }></Class>
 			</div>
 		);
 	}
