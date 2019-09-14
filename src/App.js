a4e5919145c5e0d5d9f3bd0de93a82053b86259a
张浩123 <13930333639@163.com>
2019-09-14 20:08:31 +0800

    children属性基本用法

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import './App.css';
 // 类表达式
 // import Classexopression from './components/class-expression.js';
 // 类延展操作
-import Class from './components/parameter.js';
+// import Class from './components/parameter.js';
+// childen属性
+import Childen from './components/childen-demo.js';
 
 class App extends React.Component {
 	render () {
-		let info = {
-			name: '张三',
-			age: 20,
-			gender: '男',
-			photo: 120
-		};
-		// 这样可以取出不需要的数据，只传递需要的数据
-		let { name, ...other } = info;
+		// let info = {
+		// 	name: '张三',
+		// 	age: 20,
+		// 	gender: '男',
+		// 	photo: 120
+		// };
+		// // 这样可以取出不需要的数据，只传递需要的数据
+		// let { name, ...other } = info;
 		return (
 			<div>
-				{/*将info属性传到子组件*/}
-				<Class { ...info }></Class>
-				<Class { ...other }></Class>
+				<Childen>
+					<div>你好</div>
+				</Childen>
 			</div>
 		);
 	}
